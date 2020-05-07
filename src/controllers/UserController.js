7f7d4b9cc8ae053bdf0988d8bdacae9e3856c5d3
joaov56 <vitorjao56@gmail.com>
2020-05-07 12:31:51 -0300

    Adicionado metodos para update e remocao

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -6,13 +6,50 @@ module.exports = {
         return res.json(results)
     },
 
-    async create(req, res) {
-        const { username } = req.body
+    async create(req, res, next) {
+        try {
+            const { username } = req.body
+            await knex('users').insert({
+                username
+            })
+
+            return res.status(201).send()
+        } catch (error) {
+            next(error)
+        }
+
+    },
+
+    async update(req, res, next) {
+        try {
+            const { username } = req.body;
+            const { id } = req.params;
 
 
-        await knex.insert({
-            username
-        })
+            await knex('users')
+                .update({ username })
+                .where({ id })
+
+            return res.send()
+
+        } catch (error) {
+            next(error)
+        }
+    },
+
+    async delete(req, res, next) {
+        try {
+
+            const { id } = req.params
+
+            await knex('users')
+                .where({ id })
+                .del()
+
+            return res.send()
+
+        } catch (error) {
+            next(error)
+        }
     }
-
 }
--- src/server.js
+++ src/server.js
@@ -7,6 +7,12 @@ app.use(express.json())
 app.use(routes);
 
 
+app.use((error, req, res, next) => {
+    res.status(error.status || 500)
+    res.json({ error: error.message })
+
+})
+
 app.listen(3000, () => {
     console.log("server is running");
 
